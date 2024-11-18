2fe06294777eeca5c8ed29cf3bf026b7d79c08a0
NeoMashego <mashegonoj@gmail.com>
2024-11-18 15:25:40 +0200

    import enums file and edit content

--- utils.ts
+++ utils.ts
@@ -1,10 +1,12 @@
+import { LoyaltyUser } from './enums.ts'
+
 const returningUserDisplay = document.querySelector('#returning-user');
 const userNameDisplay = document.querySelector('#user');
 const reviewTotalDisplay = document.querySelector('#reviews');
 
 
-export function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean){                      //setting parameters to be passed
-    const loyaltyIcon = isLoyalty ? '🌞' : '🔴';
+export function showReviewTotal(value: number, reviewer: string, isLoyalty: LoyaltyUser){                      //setting parameters to be passed
+    const loyaltyIcon = LoyaltyUser.GOLD_USER ? '🌞' : '🔴';
     reviewTotalDisplay.innerHTML = 'review total' + value.toString()            //innerHTML of h5 tag is expecting a string  && swopping value toString
                                     + '| last reviewed by' + reviewer           //pushing a string value
                                     + ' ' + loyaltyIcon;
