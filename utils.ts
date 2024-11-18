7e732e176aed53c5a71f318b17a8fdd2f2ab0e74
NeoMashego <mashegonoj@gmail.com>
2024-11-18 16:26:20 +0200

    added 2 export functions

--- utils.ts
+++ utils.ts
@@ -13,10 +13,24 @@ export function showReviewTotal(value: number, reviewer: string, isLoyalty: Loya
 }
 
 
-
 export function populateUser(isReturning: boolean, userName: string){
     if(isReturning == true){
         returningUserDisplay.innerHTML = 'back';
     }
     userNameDisplay.innerHTML = userName;
 }
+
+
+export function showDetails(authorityStatus: boolean | Permissions, element : HTMLDivElement, price: number) {
+    if (authorityStatus) {
+        const priceDisplay = document.createElement('div')
+        priceDisplay.innerHTML = price.toString() + '/night'
+        element.appendChild(priceDisplay)
+    }
+ }
+
+ export function makeMultiple(value: number){
+    if(value > 1 || value == 0){
+        return 's'
+    } else return ''
+ }
